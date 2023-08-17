3119705c996a503badfe1edd7eda27fcb4dd0308
Brant A <baabeln@ucdavis.edu>
2023-08-16 20:56:08 -0500

    Evaluate 7 templated

--- evaluator7.py
+++ evaluator7.py
@@ -7,4 +7,61 @@
 # 2b2. If the number of that suit is 6, take the greatest of the 5 - 5 card flush lookups
 # 2c. If there is only one suit (7 cards in that suit), then the best hand is a flush or straight flush
 #     use the computed 7-card flush lookup
-:while
+import collections
+import itertools
+from numpy import prod
+import pickle
+from typing import List
+from functools import reduce
+from card import Card
+
+flush5_dict = pickle.load(open("flush_dict.pkl", "rb"))
+
+# Get the suit string from each card
+def small_suit_calc(card: Card) -> int:
+    return (card.get_int_rep() & 61440) >> 12
+
+# Count the number of suits in the hand and return the greatest
+# Ties do not matter since the highest tie is 3/3/1 and a flush can only occur with 5 or more
+def num_suit(cards: List(Card)) -> (int, int):
+    suit_cnt = collections.Counter([small_suit_calc(card) for card in cards])
+    (small_suit, num) = suit_cnt.most_common(1)[0]
+    return (small_suit << 12, num)
+
+# Get the unique binary indicators of cards in a suit
+def _unique(cards: List[Card]) -> int:
+    return reduce(lambda x, y: x | y, [c.get_int_rep() for c in cards], 0) >> 16
+
+# Filter cards by a given suit int
+def get_suited_cards(cards: List(Card), suit: int) -> List(Card):
+    return [card for card in cards if card.get_int_rep() & suit != 0]
+
+# Use the prime numbers to get a product for unique lookup
+def prime_factorization_lookup(cards: List(Card)) -> int:
+    return prod(list(map(lambda x: x.get_int_rep() & 255, cards)))
+
+# Given five cards of a suit, look up using the 5-card flush table
+def flush_5_lookup(cards: List(Card)) -> int:
+    return flush5_dict[_unique(cards)]
+
+# Given six cards of a suit, look up each of the 5-card combinations using the 5-card flush table
+def flush_6_lookup(cards: List(Card)) -> int:
+    return min(flush_5_lookup(five_cards) for five_cards in itertools.combinations(cards, 5))
+
+# Use the 7-card flush table
+def flush_7_lookup(cards: List(Card)) -> int:
+    return flush7_dict[_unique(cards)]
+
+# Evaluate a 7-card hand
+def evalutate7_hand(cards: List(Card)) -> int:
+    (high_suit, num) = num_suit(cards)
+    if num < 5:
+        return prime_factorization_lookup(cards)
+    elif num == 5:
+        suited_cards = get_suited_cards(cards, high_suit)
+        return flush_5_lookup(suited_cards)
+    elif num == 6:
+        suited_cards = get_suited_cards(cards, high_suit)
+        return flush_6_lookup(suited_cards)
+    else:
+        return flush_7_lookup(cards)
